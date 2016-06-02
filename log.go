76ec2cd33550be2fd7019c0ea3f0de7a1c4f711b
Max Chen <beanbee@outlook.com>
2016-06-02 14:15:05 +0800

    add rotate size

--- log.go
+++ log.go
@@ -2,10 +2,11 @@
 /*
    modification history
    --------------------
-   2016/02/03, by Chen Jian, create
-   2016/02/04, by Chen Jian, add getter and setter
-   2016/02/21, by Chen Jian, add string.trimspace for progName
-   2016/03/16, by Chen Jian, set log level to 'log4go.DEBUG' in debugMode
+   2016/02/03, by beanbee, create
+   2016/02/04, by beanbee, add getter and setter
+   2016/02/21, by beanbee, add string.trimspace for progName
+   2016/03/16, by beanbee, set log level to 'log4go.DEBUG' in debugMode
+   2016/06/02, by beanbee, add rotate size
 */
 
 /*
@@ -15,9 +16,9 @@ log: encapsulation for log4go
 Usage:
     import log "github.com/beanbee/log-go"
 
-    // Two log files will be generated in ./log:
+    // Two log files will be generated in ./log
     // test.log, and test.log.wf (for log > warn)
-    // The log will rotate, and there is support for backup count
+    // The log will rotate, and with support for backup count
     logger ,err := log.NewLogger("test").SetLogDir("./log").Init()
 
     logger.Warn("warn msg")
@@ -30,7 +31,6 @@ Usage:
 package log
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -38,8 +38,10 @@ import (
 	"code.google.com/p/log4go"
 )
 
-// default log format
-const DEFAULT_LOG_FORMAT = `[%D %T] [%L] %M`
+const (
+	LOG_FORMAT_WITHOUT_SRC = "[%D %T] [%L] %M"
+	ROTATE_SIZE_DEFAULT    = 1024 * 1024 * 1024 // 1 GB
+)
 
 /*
 DESCRIPTION: struct "Logger" - log4go encapsulation
@@ -48,111 +50,61 @@ PARAMS:
   - progName: program name. Name of log file will be progName.log
   - levelStr: "DEBUG", "TRACE", "INFO", "WARNING", "ERROR", "CRITICAL"
   - logDir: directory for log. It will be created if noexist
-  - hasStdOut: whether to have stdout output
-  - when:
-      "M", minute
-      "H", hour
-      "D", day
-      "MIDNIGHT", roll over at midnight
-  - backupCount: If backupCount is > 0, when rollover is done, no more than
-      backupCount files are kept - the oldest ones are deleted.
   - enableWf: using extra log file for 'warning, error, critical' level msg
-  - debugMode: use log4go.FORMAT_DEFAULT_WITH_PID instead of DEFAULT_LOG_FORMAT
+  - enableStdout: whether to have stdout output
+  - enableStdout: use log4go.FORMAT_DEFAULT_WITH_PID instead of DEFAULT_LOG_FORMAT
 */
 type Logger struct {
+	progName     string
+	logDir       string
+	rotateSize   int
+	enableWf     bool
+	enableDebug  bool
+	enableStdout bool
+
 	log4go.Logger
-	progName    string
-	level       string
-	logDir      string
-	when        string
-	backupCount int
-	hasStdOut   bool
-	enableWf    bool
-	debugMode   bool
 }
 
 // initial new logger - use default values
 func NewLogger(progName string) *Logger {
 	return &Logger{
-		progName:    progName,
-		level:       "INFO",
-		logDir:      "log",
-		when:        "MIDNIGHT",
-		backupCount: 7,
-		hasStdOut:   false,
-		enableWf:    true,
-		debugMode:   false,
+		progName:     progName,
+		logDir:       "log",
+		rotateSize:   ROTATE_SIZE_DEFAULT,
+		enableStdout: false,
+		enableWf:     false,
+		enableDebug:  false,
 	}
 }
 
-// access private variable through getter/setter function
-func (l *Logger) SetLevel(level string) *Logger {
-	l.level = level
+func (l *Logger) SetLogDir(dir string) *Logger {
+	l.logDir = dir
 	return l
 }
 
-func (l *Logger) GetLevel() string {
-	return l.level
-}
-
-func (l *Logger) SetLogDir(logDir string) *Logger {
-	l.logDir = logDir
+func (l *Logger) SetRotateSize(size int) *Logger {
+	l.rotateSize = size
 	return l
 }
 
-func (l *Logger) GetLogDir() string {
-	return l.logDir
-}
-
-func (l *Logger) SetWhen(when string) *Logger {
-	l.when = when
+func (l *Logger) EnableStdout(enable bool) *Logger {
+	l.enableStdout = enable
 	return l
 }
 
-func (l *Logger) GetWhen() string {
-	return l.when
-}
-
-func (l *Logger) SetBackupCount(days int) *Logger {
-	l.backupCount = days
+func (l *Logger) EnableWf(enable bool) *Logger {
+	l.enableWf = enable
 	return l
 }
 
-func (l *Logger) GetBackupCount() int {
-	return l.backupCount
-}
-
-func (l *Logger) EnableWf(useWf bool) *Logger {
-	l.enableWf = useWf
+func (l *Logger) EnableDebug(enable bool) *Logger {
+	l.enableDebug = enable
 	return l
 }
 
-func (l *Logger) GetEnableWf() bool {
-	return l.enableWf
-}
-
-func (l *Logger) SetDebugMode(debug bool) *Logger {
-	l.debugMode = debug
-	return l
-}
-
-func (l *Logger) GetDebugMode() bool {
-	return l.debugMode
-}
-
-func (l *Logger) SetStdOut(useStd bool) *Logger {
-	l.hasStdOut = useStd
-	return l
-}
-
-func (l *Logger) GetStdOutMode() bool {
-	return l.hasStdOut
-}
-
-// logDirCreate(): check and create dir if nonexist
+// check and create dir if nonexist
 func logDirCreate(logDir string) error {
 	if _, err := os.Stat(logDir); os.IsNotExist(err) {
-		// create directory
 		err = os.MkdirAll(logDir, 0755)
 		if err != nil {
 			return err
@@ -162,9 +114,9 @@ func logDirCreate(logDir string) error {
 }
 
 // filenameGen(): generate filename
-func filenameGen(progName, logDir string, isErrLog bool) string {
+func filenameGen(progName, logDir string, enableWf bool) string {
 	var fileName string
-	if isErrLog {
+	if enableWf {
 		// for log file of warning, error, critical
 		fileName = filepath.Join(logDir, strings.TrimSpace(progName)+".log.wf")
 	} else {
@@ -175,30 +127,6 @@ func filenameGen(progName, logDir string, isErrLog bool) string {
 	return fileName
 }
 
-/* convert level in string to log4go level  */
-func stringToLevel(str string) log4go.LevelType {
-	var level log4go.LevelType
-
-	str = strings.ToUpper(str)
-	switch str {
-	case "DEBUG":
-		level = log4go.DEBUG
-	case "TRACE":
-		level = log4go.TRACE
-	case "INFO":
-		level = log4go.INFO
-	case "WARNING":
-		level = log4go.WARNING
-	case "ERROR":
-		level = log4go.ERROR
-	case "CRITICAL":
-		level = log4go.CRITICAL
-	default:
-		level = log4go.INFO
-	}
-	return level
-}
-
 /*
 Init - initialize log lib
 
@@ -207,57 +135,48 @@ RETURNS:
     nil, error   - if fail
 */
 func (l *Logger) Init() (*Logger, error) {
-	// check when
-	if !log4go.WhenIsValid(l.when) {
-		return nil, fmt.Errorf("invalid value of when: %s", l.when)
-	}
-
 	// check, and create dir if nonexist
 	if err := logDirCreate(l.logDir); err != nil {
-		log4go.Error("Init(), in logDirCreate(%s)", l.logDir)
 		return nil, err
 	}
 
-	// convert level from string to log4go level
-	level := stringToLevel(l.level)
-	if l.GetDebugMode() {
+	// default level: INFO
+	level := log4go.INFO
+	if l.enableDebug {
 		level = log4go.DEBUG
-	}
-
-	// create logger
-	logger := make(log4go.Logger)
-
-	// create writer for stdout
-	if l.hasStdOut {
-		logger.AddFilter("stdout", level, log4go.NewConsoleLogWriter())
 	}
 
 	// set logger format
 	logFormat := func(enableDebug bool) string {
 		if enableDebug {
-			return log4go.FORMAT_DEFAULT_WITH_PID
+			return log4go.FORMAT_DEFAULT
 		}
-		return DEFAULT_LOG_FORMAT
-	}(l.debugMode)
+		return LOG_FORMAT_WITHOUT_SRC
+	}(l.enableDebug)
+
+	// create logger
+	logger := make(log4go.Logger)
+
+	// create writer for stdout
+	if l.enableStdout {
+		logger.AddFilter("stdout", level, log4go.NewConsoleLogWriter())
+	}
 
 	// create file writer for all log
 	fileName := filenameGen(l.progName, l.logDir, false)
-	logWriter := log4go.NewTimeFileLogWriter(fileName, l.when, l.backupCount)
-	if logWriter == nil {
-		return nil, fmt.Errorf("error in log4go.NewTimeFileLogWriter(%s)", fileName)
-	}
+	logWriter := log4go.NewFileLogWriter(fileName, true)
 	logWriter.SetFormat(logFormat)
+	logWriter.SetRotateSize(l.rotateSize)
+	logWriter.SetRotateDaily(true)
 	logger.AddFilter("log", level, logWriter)
 
-	// create file writer for warning and fatal log
 	if l.enableWf {
 		fileNameWf := filenameGen(l.progName, l.logDir, true)
-		logWriter = log4go.NewTimeFileLogWriter(fileNameWf, l.when, l.backupCount)
-		if logWriter == nil {
-			return nil, fmt.Errorf("error in log4go.NewTimeFileLogWriter(%s)", fileNameWf)
-		}
-		logWriter.SetFormat(logFormat)
-		logger.AddFilter("log_wf", log4go.WARNING, logWriter)
+		logWriterWf := log4go.NewFileLogWriter(fileNameWf, true)
+		logWriterWf.SetFormat(logFormat)
+		logWriterWf.SetRotateSize(l.rotateSize)
+		logWriterWf.SetRotateDaily(true)
+		logger.AddFilter("log_wf", log4go.WARNING, logWriterWf)
 	}
 
 	// set Logger
